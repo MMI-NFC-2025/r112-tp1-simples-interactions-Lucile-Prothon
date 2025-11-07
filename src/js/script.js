df978373a3afcf55c36744d0c3b0ff69a013568d
Lucile-Prothon <lucile.prothon@edu.univ-fcomte.fr>
2025-11-07 15:39:35 +0100

    Ajout du code

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 // TODO
+
+// Texte en rouge
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+aMettreEnRouge.style.color = "red";
+
+// Clique du bouton en rouge
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAClick.addEventListener("click", function() {
+    enRougeSuiteAClick.style.color = "red";
+});
+
+// h2 en rouge
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((elm)=>{
+    elm.addEventListener("click",(evt)=>{
+        evt.target.style.color = "red";
+    });
+});
+
